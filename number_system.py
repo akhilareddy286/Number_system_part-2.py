35d3ded7c26227450f15f094dd0071b7749daca4
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-04-15 11:21:21 +0530

    Add files via upload

---
+++ number_system.py
@@ -0,0 +1,44 @@
+# from fractions import Fraction as F
+# print(F(8,2))
+# print(F(5.6))
+# print(F('5.6'))
+# print(F("5.6"))
+# a=F(5.6)
+# print(a,type(a))
+# print(F(4,0))
+# b=4/0
+# a=F(5,25)
+# print(a)
+
+# a=4+8j
+# print(a,type(a))
+# print(a.real)
+# print(a.imag)
+# c=5*5.6j
+# print(c,type(c))
+# print(c.real)
+# print(c.imag)
+
+# print(1e0)
+# print(4e0)
+# print(2e2)
+# print(1e1)
+# print(3e3)
+# print(10e2)
+# print(5.6e1)
+# print(9.7e3)
+# print(4.5e2)
+# print(10.5e4)
+
+# print(6*8)
+# print(5*4)
+# print(2**3)
+# print(5**3)
+# print(5.6**1)
+# print(5.6**2)
+# print(5.6**3)
+
+import math
+print(math.exp(1))
+print(math.exp(2))
+print(math.exp(3))
